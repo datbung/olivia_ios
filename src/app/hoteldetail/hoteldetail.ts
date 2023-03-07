4e2032208b5e336bf8555766672bdce1eb15cd5f
datbung <bungdat@gmail.com>
2023-03-07 16:03:16 +0700

    .

--- src/app/hoteldetail/hoteldetail.ts
+++ src/app/hoteldetail/hoteldetail.ts
@@ -241,7 +241,18 @@ export class HotelDetailPage implements OnInit {
       // imgLoaderConfigService.setConcurrency(10);
       this.platform.resume.subscribe(async()=>{
         this.zone.run(()=>{
-          this.loaddata(true);
+          
+          this.subscription = this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd && (event.url.indexOf('/hoteldetail/') != -1) ) {
+              let key = 'hoteldetail_' + this.HotelID;
+              if((this.searchhotel.keySearchHotelDetail && this.searchhotel.keySearchHotelDetail != key) || !this.searchhotel.keySearchHotelDetail){
+                this.searchhotel.keySearchHotelDetail = key;
+                this.loaddata(true);
+              }
+            }else{
+              console.log(event instanceof NavigationEnd);
+            }
+          })
         })
       })
 
@@ -377,6 +388,7 @@ export class HotelDetailPage implements OnInit {
       }
 
   loaddata(isResume){
+    
       if(this.isLoadingData){
         this.isLoadingData = false;
         return;
@@ -1146,7 +1158,7 @@ export class HotelDetailPage implements OnInit {
           se.name = jsondata.Name;
           se.searchhotel.gaHotelDetail = {...jsondata};
           se.searchhotel.gaHotelDetail.RatingValue = se.searchhotel.gaHotelDetail.Rating/10;
-          let key = 'hoteldetail_' + se.HotelID+"_"+se.cindisplay+"_"+se.coutdisplay+"_"+(se.searchhotel.adult ||0)+"_"+(se.searchhotel.child ||0)+"_"+(se.searchhotel.roomnumber ||0);
+          let key = 'hoteldetail_' + se.HotelID;
           if((se.searchhotel.keySearchHotelDetail && se.searchhotel.keySearchHotelDetail != key) || !se.searchhotel.keySearchHotelDetail){
             se.searchhotel.keySearchHotelDetail = key;
             se.gf.logEventFirebase('',se.searchhotel, 'hoteldetail', 'view_item', 'Hotels');
