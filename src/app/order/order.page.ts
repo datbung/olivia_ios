30794719fdc512c86a59fb8b24daeb0e7c24a3b4
noname <32832607+nonameuser@users.noreply.github.com>
2023-05-15 20:25:05 +0700

    up

--- src/app/order/order.page.ts
+++ src/app/order/order.page.ts
@@ -6639,7 +6639,6 @@ export class OrderPage {
     }else{
       this.zone.run(() => {
         trip.bookingjson[0].RequestCheckin=0;
-        trip.ischeckinOnl=true;
         alert("Chuyến bay trong khung đóng chuyến. Qúy khách vui lòng làm thủ tục tại kios hoặc quầy checkin tại sân bay");
       })
 
